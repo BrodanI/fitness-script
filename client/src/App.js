6d47937cf1aae6deda76b84e60a22c273c133d36
BrodanI <BrodanI@users.noreply.github.com>
2021-03-21 23:13:05 -0700

    Added basic npm packages needed. Created basic folder structure and files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import React from "react";
+import { Component } from "react";
+import axios from "axios";
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+
+class App extends Component() {
+  return() {
+    render(
+      <div className="App">
+        <BrowserRouter>
+          <Router className="App">
+            <Header />
+            <Switch>
+
+              <Route path='/home' exact component={Home} />
+              <Route path='/login' component={Login} />
+
+            </Switch>
+          </Router>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
